9fb32e91c2a67d8b4e02aefa856dcae3f5825f8a
VladislavMikulich <160238253+VladislavMikulich@users.noreply.github.com>
2024-02-16 16:12:20 +0100

    Feature/base template and header (#1)
    Created base template for "Topics" page and header

---
+++ components/Topic.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h2>{{ props.title }}</h2>
+    <p>{{ props.content }}</p>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+section p {
+  font-size: 20px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sections-wrapper">
+    <Topic
+      v-for="(section, index) in sectionsData"
+      :key="index"
+      :title="section.title"
+      :content="section.content"
+    />
+  </div>
+</template>
+
+<script setup>
+import { SECTIONS_DATA as sectionsData } from "../constants/"
+</script>
+
+<style scoped>
+.sections-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+}
+</style>
